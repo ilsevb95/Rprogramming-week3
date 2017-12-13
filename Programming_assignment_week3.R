fcecb6918d6892515f817101158e13041a0f889c
ilsevb95 <34510890+ilsevb95@users.noreply.github.com>
2017-12-13 14:15:30 +0100

    Add files via upload

---
+++ Programming_assignment_week3.R
@@ -0,0 +1,44 @@
+#########Peer graded assignment week 3 ####################
+#Programming assignment
+#Lexical scoping
+
+######LIBRARIES###########################################
+library(dplyr)                                           #
+##########################################################
+
+#creating the cache matrix
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse= getinverse)
+}
+  
+#function to search for cache data or compute the inverse of matrix
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
+
+#create a matrix
+y = matrix(rnorm(n=100, mean=10, sd=2),
+           nrow=10, ncol=10, byrow=T)
+y1 = makeCacheMatrix(y)
+
+#test the inverse function
+cacheSolve(y1)
+
